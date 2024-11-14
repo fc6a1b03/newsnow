5706ccbc412e54dd57069ed63b0f5a9e1125b69a
alllowMultiQueries <asd1161575755@outlook.com>
2024-11-14 08:38:26 +0800

    Update fetch.ts

--- server/utils/fetch.ts
+++ server/utils/fetch.ts
@@ -1,5 +1,5 @@
-import { $fetch } from "ofetch"
-import { ProxyAgent } from "undici"
+import {$fetch} from "ofetch"
+import {ProxyAgent} from "undici"
 
 // export const myFetch = $fetch.create({
 //   headers: {
@@ -9,22 +9,24 @@ import { ProxyAgent } from "undici"
 //   retry: 3,
 // })
 
-// 从环境变量中读取代理地址
+// 代理地址
 const proxyUrl = process.env.HTTP_PROXY || process.env.HTTPS_PROXY
-
-// 根据是否有代理地址来创建配置
-const myFetchOptions = {
-  headers: {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
-  },
-  timeout: 5000,
-  retry: 3
+/**
+ *
+ * @param url 请求地址
+ * @param options 请求选项
+ * @param useProxy 使用代理，默认：false不使用
+ */
+export const myFetch = (url: any, options = {}, useProxy = false) => {
+    return $fetch(url,
+        {
+            ...options,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+            },
+            retry: 3,
+            timeout: 5000,
+            dispatcher: useProxy && proxyUrl ? new ProxyAgent(proxyUrl) : undefined,
+        }
+    )
 }
-
-// 如果有代理地址，添加 dispatcher
-if (proxyUrl) {
-  myFetchOptions.dispatcher = new ProxyAgent(proxyUrl)
-}
-
-// 创建 $fetch 实例
-export const myFetch = $fetch.create(myFetchOptions)
