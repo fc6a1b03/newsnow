31ab5ad568ed30c15491fd484826795f3b729625
alllowMultiQueries <asd1161575755@outlook.com>
2024-11-14 09:06:03 +0800

    Update producthunt.ts

--- server/sources/producthunt.ts
+++ server/sources/producthunt.ts
@@ -3,7 +3,7 @@ import type { NewsItem } from "@shared/types"
 
 export default defineSource(async () => {
   const baseURL = "https://www.producthunt.com/"
-  const html: any = await myFetch(baseURL, true)
+  const html: any = await myFetch(baseURL, {}, true)
   const $ = cheerio.load(html)
   const $main = $("[data-test=homepage-section-0] [data-test^=post-item]")
   const news: NewsItem[] = []
